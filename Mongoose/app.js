019dd0418d50890f2dac31a17412d68f6b3d1e3a
srihari-sridharan <srihari.sridharan.r@gmail.com>
2015-08-15 07:11:11 +0530

    Mongoose

---
+++ Mongoose/app.js
@@ -0,0 +1,12 @@
+// var mongoose = require('mongoose');
+// var mongoConnectionString = "mongodb://localhost:27017/MongoosePM";
+//
+// mongoose.connect(mongoConnectionString);
+// mongoose.connection.close(function() {
+//   console.log("Closed default connection!");
+// });
+//
+// var c = mongoose.createConnection(mongoConnectionString);
+// c.close(function() {
+//   console.log("Closed new connection!");
+// });
